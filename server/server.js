bc4ee5676bf5270d66769a0722cb1593f3a11ce7
Yasin Erkan <yasinerkan@gmail.com>
2025-11-09 16:15:20 +0100

    chore: vercel config for api

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import {clerkMiddleware} from '@clerk/express';
+import clerkWebhooks from './controllers/clerkWebHooks.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(cors());
+
+//middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+//API to listenClerk Webhook
+app.use('/api/clerk', clerkWebhooks);
+
+app.get('/', (req, res) => res.send('API is working perfectly'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port  ${PORT}`));
